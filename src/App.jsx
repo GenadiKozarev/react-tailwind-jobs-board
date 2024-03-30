aa5b3c33bd1e9de28eb291b90eebd11fe50206b0
Genadi <genadi.kozarev@gmail.com>
2024-03-30 22:26:40 +0000

    add fake jobs data and use it dynamically in job listing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
 import Navbar from './components/Navbar';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <HomeCards />
+            <JobListings />
         </>
     );
 };
